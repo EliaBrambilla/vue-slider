dc7bf85b738390290de58b7ad9f9caa43ff4eb42
EliaBrambilla <elia.brambilla@icloud.com>
2022-11-15 09:54:25 +0100

    aggiungo html e js

---
+++ js/main.js
@@ -0,0 +1,61 @@
+const { createApp } = Vue;
+
+
+createApp({
+    data() {
+        return{
+            slides: [
+                {
+                    image: 'https://picsum.photos/300/300',
+                    title: 'Marvel\'s Spiderman Miles Morale',
+                    text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+                }, {
+                    image: 'https://picsum.photos/300/300',
+                    title: 'Ratchet & Clank: Rift Apart',
+                    text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+                }, {
+                    image: 'https://picsum.photos/300/300',
+                    title: 'Fortnite',
+                    text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+                }, {
+                    image: 'https://picsum.photos/300/300',
+                    title: 'Stray',
+                    text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+                }, {
+                    image: 'https://picsum.photos/300/300',
+                    title: "Marvel's Avengers",
+                    text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+                }
+            ], 
+            activeImage: 0
+        }
+    },
+    methods: {
+        prevSlide(){
+            if(this.activeImage === 0){
+                this.activeImage = this.slides.length - 1;
+            }else{
+                this.activeImage--;
+            }
+        }, 
+        nextSlide(){
+            if(this.activeImage === this.slides.length - 1){
+                this.activeImage = 0;
+            }else{
+                this.activeImage++;
+            }
+        }, 
+        changeSlide(index){
+            this.activeImage = index
+        },
+        mounted(){
+            let timer = setInterval(this.nextSlide, 3000)
+        },
+        stopPreview(){
+            clearInterval(timer)
+        }, 
+        startPreview(){
+            timer = setInterval(this.nextSlide, 3000)
+        }
+    }
+}).mount('#app');
